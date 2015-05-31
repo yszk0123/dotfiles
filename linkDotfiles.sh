ca020940255c053fb8ecfb8d10f85e3146b034d1
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-06-01 00:12:31 +0900

    linkDotfiles.shで.irbrcと.bash_profileもリンクするように

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+# ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
+# ln -sf ~/dotfiles/Brewfile ~/Brewfile
 # ln -sf ~/dotfiles/colors ~/.vim
 # ln -sf ~/dotfiles/ftdetect ~/.vim
 # ln -sf ~/dotfiles/indent ~/.vim
-# ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-# ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
-# ln -sf ~/dotfiles/Brewfile ~/Brewfile
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.irbrc ~/.irbrc
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
