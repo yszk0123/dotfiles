130ec614d66ac65cf334202fc39ae103afb34d8d
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-06-15 15:29:48 +0900

    Add .gitconfig

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -7,6 +7,7 @@
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.gemrc ~/.gemrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.irbrc ~/.irbrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
