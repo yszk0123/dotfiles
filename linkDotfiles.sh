ac74f4c65fec92aa5ed670942a5b96b579c12460
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-08-13 23:03:01 +0900

    feat: シンボリックリンク上書き前に確認を取る

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
-# ln -sf ~/dotfiles/.wgetrc ~/.wgetrc
-# ln -sf ~/dotfiles/Brewfile ~/Brewfile
-# ln -sf ~/dotfiles/colors ~/.vim
-# ln -sf ~/dotfiles/ftdetect ~/.vim
-# ln -sf ~/dotfiles/indent ~/.vim
+# ln -si ~/dotfiles/.wgetrc ~/.wgetrc
+# ln -si ~/dotfiles/Brewfile ~/Brewfile
+# ln -si ~/dotfiles/colors ~/.vim
+# ln -si ~/dotfiles/ftdetect ~/.vim
+# ln -si ~/dotfiles/indent ~/.vim
 
-ln -sf ~/dotfiles/.bundle ~/.bundle
-ln -sf ~/dotfiles/.git_template ~/.git_template
+ln -si ~/dotfiles/.bundle ~/.bundle
+ln -si ~/dotfiles/.git_template ~/.git_template
 
-ln -sf ~/dotfiles/.ansible.cfg ~/.ansible.cfg
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/.ctags ~/.ctags
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -si ~/dotfiles/.ansible.cfg ~/.ansible.cfg
+ln -si ~/dotfiles/.zprofile ~/.zprofile
+ln -si ~/dotfiles/.ctags ~/.ctags
+ln -si ~/dotfiles/.gitconfig ~/.gitconfig
+ln -si ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -si ~/dotfiles/.vimrc ~/.vimrc
+ln -si ~/dotfiles/.zshrc ~/.zshrc
 
 if [ -s ~/dotfiles/my_local_mode* ]; then
-  ln -sf ~/dotfiles/.gemrc ~/.gemrc
-  ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-  ln -sf ~/dotfiles/.irbrc ~/.irbrc
+  ln -si ~/dotfiles/.gemrc ~/.gemrc
+  ln -si ~/dotfiles/.gvimrc ~/.gvimrc
+  ln -si ~/dotfiles/.irbrc ~/.irbrc
 fi
