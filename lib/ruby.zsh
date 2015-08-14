7c85b2d51434fa1fc75e00d6f9b41d233f9628d8
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-08-14 09:19:12 +0900

    fix: bundleコマンドの遅延ロード化。aliasもロード前から使用可能に

--- lib/ruby.zsh
+++ lib/ruby.zsh
@@ -3,21 +3,21 @@ if is_exists 'rbenv'; then
     unset -f rbenv
     unset -f ruby
     unset -f irb
+    unset -f bundle
 
     eval "$(rbenv init -)"
-
-    if is_exists 'bundle'; then
-      alias be='bundle exec'
-      alias berc='bundle exec rails c'
-      alias bers='bundle exec rails s'
-      alias ber='bundle exec rspec'
-      alias staging_be='RAILS_ENV=staging bundle exec'
-    fi
   }
 
   rbenv() { load_ruby; rbenv "$@"; }
   ruby() { load_ruby; ruby "$@"; }
   irb() { load_ruby; irb "$@"; }
+  bundle() { load_ruby; bundle "$@"; }
+
+  alias be='bundle exec'
+  alias berc='bundle exec rails c'
+  alias bers='bundle exec rails s'
+  alias ber='bundle exec rspec'
+  alias staging_be='RAILS_ENV=staging bundle exec'
 fi
 
 # vim:set ft=zsh:
