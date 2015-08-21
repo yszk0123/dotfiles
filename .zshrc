2756ce65ba58aa79bc9d34c748176bdded5766d1
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-08-21 23:14:39 +0900

    refactor(zsh): alias移動

--- .zshrc
+++ .zshrc
@@ -23,27 +23,4 @@ for name in alias clipboard complete docker history lv node os prompt ruby setti
   source "$HOME/dotfiles/lib/$name.zsh"
 done
 
-# Alias {{{
-alias la='ls -a'
-alias ll='ls -l'
-alias pu='pushd'
-alias po='popd'
-
-## ghq & peco
-alias ghqp='cd $(ghq list -p | peco)'
-alias ghqp-open='gh-open $(ghq list -p | peco)'
-
-## 上書き確認
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
-
-##alias mkdir='mkdir -p'
-
-# グローバルエイリアス {{{
-alias -g L='| less'
-alias -g G='| grep'
-# }}}
-# }}}
-
 # vim:set ft=zsh:
--- lib/alias.zsh
+++ lib/alias.zsh
@@ -1,11 +1,37 @@
+# {{{
 # cf. [その他のオプションの調べ方](http://qiita.com/uasi/items/c4288dd835a65eb9d709#1-5)
 # cd ~abc => /a/b/c
 # hash -d abc=/a/b/c
-
 hash -d d=~/dotfiles
 hash -d w=~/work
 hash -d D=~/Dropbox
 hash -d Dg=~/Dropbox/git
 hash -d sg=~/src/github.com
+# }}}
+
+# {{{
+alias la='ls -a'
+alias ll='ls -l'
+alias pu='pushd'
+alias po='popd'
+# }}}
+
+# ghq & peco {{{
+alias ghqp='cd $(ghq list -p | peco)'
+alias ghqp-open='gh-open $(ghq list -p | peco)'
+# }}}
+
+# 上書き確認 {{{
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+# }}}
+
+#alias mkdir='mkdir -p'
+
+# グローバルエイリアス {{{
+alias -g L='| less'
+alias -g G='| grep'
+# }}}
 
 # vim:set ft=zsh:
