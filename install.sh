92e65c49c540aeac1d8ca8845eb9a8562d084a56
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-09-13 14:53:10 +0900

    chore(install): Add installation script for nvm

--- install.sh
+++ install.sh
@@ -35,3 +35,12 @@ if [ ! -s "$HOME/.tmux/plugins/tpm/tpm" ]; then
       ;;
   esac
 fi
+
+if [ ! -s "$HOME/.nvm/nvm.sh" ]; then
+  read -p "Do you wish to install nvm? [YyNn]" yn
+  case $yn in
+    [Yy]* )
+      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.26.1/install.sh | bash
+      ;;
+  esac
+fi
