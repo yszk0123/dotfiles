4945dd2bed5fb231c48ea2dcdf3695914359afa0
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-09-15 09:12:26 +0900

    chore(linkDotfiles): Refactor using for statement

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -5,10 +5,6 @@
 # ln -si ~/dotfiles/ftdetect ~/.vim
 # ln -si ~/dotfiles/indent ~/.vim
 
-ln -si ~/dotfiles/.atom ~/.atom
-ln -si ~/dotfiles/.bundle ~/.bundle
-ln -si ~/dotfiles/.git_template ~/.git_template
-
 # $ZDOTDIRが.zprofile中で指定される場合は、$HOME/.zprofileが読み込まれる（はず）
 # 念のため両方に用意しておく
 : ${ZDOTDIR:=~/.zsh}
@@ -16,14 +12,12 @@ ln -si ~/dotfiles/.zprofile ~/.zprofile
 ln -si ~/dotfiles/.zprofile "$ZDOTDIR/.zprofile"
 ln -si ~/dotfiles/.zshrc "$ZDOTDIR/.zshrc"
 
-ln -si ~/dotfiles/.ansible.cfg ~/.ansible.cfg
-ln -si ~/dotfiles/.ctags ~/.ctags
-ln -si ~/dotfiles/.gitconfig ~/.gitconfig
-ln -si ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -si ~/dotfiles/.vimrc ~/.vimrc
+for target in .atom .bundle .git_template .ansible.cfg .ctags .gitconfig .tmux.conf .vimrc; do
+  ln -si ~/dotfiles/$target ~/$target
+done
 
 if [ -s ~/dotfiles/my_local_mode* ]; then
-  ln -si ~/dotfiles/.gemrc ~/.gemrc
-  ln -si ~/dotfiles/.gvimrc ~/.gvimrc
-  ln -si ~/dotfiles/.irbrc ~/.irbrc
+  for target in .gemrc .gvimrc .irbrc; do
+    ln -si ~/dotfiles/$target ~/$target
+  done
 fi
