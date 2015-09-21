6f622ab6d5a15065fe91b44c88f827d67ff3d881
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-09-21 10:52:22 +0900

    fix(zsh): Use default version of Node.js

--- lib/node.zsh
+++ lib/node.zsh
@@ -14,7 +14,7 @@ if [ -s "$NVM_DIR/nvm.sh" ]; then
     echo 'Loading nvm/npm/node...'
 
     source "$NVM_DIR/nvm.sh"
-    nvm use 4.0.0
+    nvm use default
 
     # Enable tab-completion
     [[ -f "$NVM_DIR/bash_completion" ]] && . "$NVM_DIR/bash_completion"
