d2efc11300c21112270c2179f6746e92183de919
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-09-30 23:32:15 +0900

    chore(link): Don't make symbolic links in target directories when link dotfiles

--- linkDotfiles.sh
+++ linkDotfiles.sh
@@ -8,17 +8,17 @@
 # $ZDOTDIRが.zprofile中で指定される場合は、$HOME/.zprofileが読み込まれる（はず）
 # 念のため両方に用意しておく
 : ${ZDOTDIR:=~/.zsh}
-ln -si ~/dotfiles/.zprofile ~/.zprofile
-ln -si ~/dotfiles/.zprofile "$ZDOTDIR/.zprofile"
-ln -si ~/dotfiles/.zshrc "$ZDOTDIR/.zshrc"
+ln -sni ~/dotfiles/.zprofile ~/.zprofile
+ln -sni ~/dotfiles/.zprofile "$ZDOTDIR/.zprofile"
+ln -sni ~/dotfiles/.zshrc "$ZDOTDIR/.zshrc"
 
 for target in .atom .bundle .git_template .ansible.cfg .ctags .gitconfig .tmux.conf .vimrc .rspec; do
-  ln -si ~/dotfiles/$target ~/$target
+  ln -sni ~/dotfiles/$target ~/$target
 done
-ln -si ~/dotfiles/.vimrc ~/.nvimrc
+ln -sni ~/dotfiles/.vimrc ~/.nvimrc
 
 if [ -s ~/dotfiles/my_local_mode* ]; then
   for target in .gemrc .gvimrc .irbrc; do
-    ln -si ~/dotfiles/$target ~/$target
+    ln -sni ~/dotfiles/$target ~/$target
   done
 fi
