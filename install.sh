d750857354b96a3b73962041b8cb935fd45dad79
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-10-17 11:53:19 +0900

    chore(install): Add installation of gh-open

--- install.sh
+++ install.sh
@@ -58,6 +58,13 @@ if ! is_exists 'ghq'; then
   esac
 fi
 
+if ! is_exists 'gh-open'; then
+  read -p "Do you wish to install gh-open? [YyNn]" yn
+  case $yn in
+    [Yy]* ) go get https://github.com/typester/gh-open ;;
+  esac
+fi
+
 if ! is_exists 'peco'; then
   read -p "Do you wish to install ghq? [YyNn]" yn
   case $yn in
