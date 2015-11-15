96db6812b0bf062ace4894936cf7375b477b6f5b
Yuji Suzuki <y0suzuki0123@gmail.com>
2015-11-15 23:45:43 +0900

    feat(git, zsh): Add aliases and functions for git commands

--- lib/vcs.zsh
+++ lib/vcs.zsh
@@ -1,11 +1,13 @@
 # git {{{
-### ref: [【翻訳】Gitコマンドラインショートカット](http://postd.cc/git-command-line-shortcuts/)
+### cf. [【翻訳】Gitコマンドラインショートカット](http://postd.cc/git-command-line-shortcuts/)
+# Aliases {{{
 alias ga='git add'
 alias gaA='git add -A'
 alias gb='git branch'
 alias gbd='git branch -d '
 alias gc='git commit'
 alias gca='git commit --amend'
+alias gcar='git commit --amend --reuse-message=HEAD'
 alias gcm='git commit -m'
 alias gco='git checkout'
 alias gcob='git checkout -b'
@@ -15,16 +17,19 @@ alias gd='git diff'
 alias gdH1='git diff HEAD~'
 alias gdH='git diff HEAD'
 alias gi='git init'
-alias gld='git log --pretty=format:"%h %ad %s" --date=short --all'
+alias gl='git log --pretty=format:"%C(yellow)%h %Cblue%ad %Creset%s" --date=short --all'
 alias glg='git log --graph --oneline --decorate --all'
-alias glo='git log'
+alias gll='git log'
+alias gref='git reflog'
 alias gm='git merge'
 alias gpic='git cherry-pick'
 alias gpul='git pull'
 alias gpus='git push'
 alias greb='git rebase'
+alias grebi='git rebase -i'
 alias gres='git reset'
 alias grev='git revert'
+alias grem='git remote'
 alias gs='git status'
 alias gss='git status -s'
 alias gst='git stash'
@@ -33,6 +38,25 @@ alias gstd='git stash drop'
 alias gstl='git stash list'
 alias gstp='git stash pop'
 alias gt='git tag'
+# }}}
+
+# Advanced shortcuts {{{
+# cf. [mathiasbynens/dotfiles](https://github.com/mathiasbynens/dotfiles/blob/master/.gitconfig)
+# Find branches containing commit
+gfb() { git branch -a --contains "$1" }
+
+# Find tags containing commit
+gft() { git describe --always --contains "$1" }
+
+# Find commits by source code
+gfc() { git log --pretty=format:'%C(yellow)%h %Cblue%ad  %Creset%s %Cgreen%cn %Cred%d' --decorate --date=short -S"$1" }
+
+# Find commits by commit message
+gfm() { git log --pretty=format:'%C(yellow)%h %Cblue%ad  %Creset%s %Cgreen%cn %Cred%d' --decorate --date=short --grep="$1" }
+
+# Interactive rebase with the given number of latest commits
+grebn() { git rebase -i HEAD~$1 }
+# }}}
 # }}}
 
 # vim:set ft=zsh:
