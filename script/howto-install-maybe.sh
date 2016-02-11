85723cc57f5dbc17477cc4612130b1c467d178c6
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-02-11 15:09:05 +0900

    feat(script): add install scripts

---
+++ script/howto-install-maybe.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# cf. [p-e-w/maybe: :rabbit2: See what a program does before deciding whether you really want it to happen.](https://github.com/p-e-w/maybe)
+
+pip install maybe || pip3 install maybe
+
---
+++ script/howto-update-neovim.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# cf. https://github.com/neovim/homebrew-neovim/blob/master/README.md
+
+brew update
+brew upgrade neovim
+
