6be4a71bc06c2b5e9c05a6c89e87e52662dc6295
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-08 17:47:35 +0900

    refactor(scripts/install): move plain commands to scripts/install/plain.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,35 +7,7 @@ if [ -z "$ZDOTDIR" ]; then
   exit 1
 fi
 
-for name in zsh tmux brew go npm pip; do
+for name in zsh tmux plain brew go npm pip; do
   echo_color "installing $name commands ..." "32"
   source "$HOME/dotfiles/scripts/install/$name.sh"
 done
-
-if ! is_exists "nodebrew"; then
-  read -p "Do you wish to install nodebrew? [YyNn]" yn
-  case $yn in
-    [Yy]* )
-      curl -L git.io/nodebrew | perl - setup
-      ;;
-  esac
-fi
-
-if ! is_exists "fzf"; then
-  read -p "Do you wish to install fzf? [YyNn]" yn
-  case $yn in
-    [Yy]* )
-      git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-      ~/.fzf/install
-      ;;
-  esac
-fi
-
-if [ ! -s "$HOME/.vim/autoload/plug.vim" ]; then
-  read -p "Do you wish to install plug.vim? [YyNn]" yn
-  case $yn in
-    [Yy]* )
-      curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-      ;;
-  esac
-fi
---
+++ scripts/install/plain.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if ! is_exists "nodebrew"; then
+  read -p "Do you wish to install nodebrew? [YyNn]" yn
+  case $yn in
+    [Yy]* )
+      curl -L git.io/nodebrew | perl - setup
+      ;;
+  esac
+fi
+
+if ! is_exists "fzf"; then
+  read -p "Do you wish to install fzf? [YyNn]" yn
+  case $yn in
+    [Yy]* )
+      git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+      ~/.fzf/install
+      ;;
+  esac
+fi
+
+if [ ! -s "$HOME/.vim/autoload/plug.vim" ]; then
+  read -p "Do you wish to install plug.vim? [YyNn]" yn
+  case $yn in
+    [Yy]* )
+      curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+      ;;
+  esac
+fi
