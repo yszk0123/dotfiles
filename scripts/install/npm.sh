7e05b2be43868d47a1d0ed63105920f00b66f056
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-08 17:04:40 +0900

    refactor(scripts/install): generalize install scripts

--- scripts/install/npm.sh
+++ scripts/install/npm.sh
@@ -1,11 +1,24 @@
-#!/bin/bash
+#!/bin/sh
+
+INSTALLED_GLOBAL_PACKAGES=$(npm ls -g)
+
+function is_installed() {
+  local result=$(echo "$INSTALLED_GLOBAL_PACKAGES" | grep "^$1$")
+  if [ -z "$result" ]; then
+    return 1;
+  else
+    return 0;
+  fi
+}
 
 # cf. https://github.com/stevemao/diff-so-fancy
-if ! is_exists 'diff-so-fancy'; then
-  read -p "Do you wish to install diff-so-fancy? [YyNn]" yn
+for target in diff-so-fancy; do
+  [ is_installed $target ] && continue
+
+  read -p "Do you wish to install $target? [YyNn]" yn
   case $yn in
     [Yy]* )
-      npm install -g diff-so-fancy
+      npm install -g "$target"
       ;;
   esac
-fi
+done
--- scripts/install/pip.sh
+++ scripts/install/pip.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+INSTALLED_PIP_PACKAGES="$(pip list)"
+CURRENT_PIP=pip3
+# CURRENT_PIP=pip
+
+function is_installed() {
+  local result=$(echo "$INSTALLED_PIP_PACKAGES" | grep "^$1$")
+  if [ -z "$result" ]; then
+    return 1;
+  else
+    return 0;
+  fi
+}
+
 # cf. [p-e-w/maybe: :rabbit2: See what a program does before deciding whether you really want it to happen.](https://github.com/p-e-w/maybe)
-if ! is_exists 'maybe'; then
-  read -p "Do you wish to install maybe? [YyNn]" yn
+for target in maybe; do
+  [ is_installed $target ] && continue
+
+  read -p "Do you wish to install $target? [YyNn]" yn
   case $yn in
     [Yy]* )
-      pip install maybe || pip3 install maybe
+      $CURRENT_PIP install "$target"
       ;;
   esac
-fi
+done
