50d68ea29c60ba1f5459f13748016941e318ce24
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-16 04:14:49 +0900

    feat(git): rename render-diff to devtools-render-diff

--- scripts/browse-diff-remote-branch-head.sh
+++ scripts/browse-diff-remote-branch-head.sh
@@ -2,4 +2,4 @@
 # require: gitlab.com/yszk0123/devtools
 # require: brew install browser
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
-render-diff origin/$BRANCH | browser
+devtools-render-diff origin/$BRANCH | browser
