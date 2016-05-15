507d5515b56702fe8cc3317ef3e03373d2e3be60
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-05-16 03:42:05 +0900

    feat(gitlab): add a script to create a new project

---
+++ gitlab/create-new-project.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Note: set gitlab private token to GITLAB_PRIVATE_TOKEN (environment variable)
+PROJECT_NAME="$1"
+
+curl -v -X POST -H "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" -d name="$PROJECT_NAME" "https://gitlab.com/api/v3/projects"
