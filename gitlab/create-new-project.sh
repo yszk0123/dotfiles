166350290bbe7ef80cceb8c593519515a99cdb48
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-06-13 00:00:25 +0900

    feat(gitlab): add argument validations

--- gitlab/create-new-project.sh
+++ gitlab/create-new-project.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # Note: set gitlab private token to GITLAB_PRIVATE_TOKEN (environment variable)
 PROJECT_NAME="$1"
+[ -z "$PROJECT_NAME" ] && echo PROJECT_NAME required && exit 1
 
 curl -v -X POST -H "PRIVATE-TOKEN: $GITLAB_PRIVATE_TOKEN" -d name="$PROJECT_NAME" "https://gitlab.com/api/v3/projects"
--- gitlab/make-dir-and-create-new-project.sh
+++ gitlab/make-dir-and-create-new-project.sh
@@ -3,6 +3,8 @@
 USER_NAME="$1"
 PROJECT_NAME="$2"
 PROJECT_DIR="$HOME/src/gitlab.com/$USER_NAME/$PROJECT_NAME"
+[ -z "$USER_NAME" ] && echo USER_NAME required && exit 1
+[ -z "$PROJECT_NAME" ] && echo PROJECT_NAME required && exit 1
 
 mkdir -p "$PROJECT_DIR"
 cd "$PROJECT_DIR"
