4a0f9001998c11dec381c615d6ca2623f9672749
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-06-28 05:51:07 +0900

    feat(git): update aliases for git command

--- zsh/vcs.zsh
+++ zsh/vcs.zsh
@@ -1,9 +1,12 @@
 # git {{{
 ### cf. [【翻訳】Gitコマンドラインショートカット](http://postd.cc/git-command-line-shortcuts/)
+### cf. https://github.com/GitAlias/gitalias
 # Aliases {{{
+alias g=git
 alias ga='git add'
 alias gaA='git add -A'
 alias gau='git add -u'
+alias ge='git exec '
 
 alias gb='git branch'
 alias gbd='git branch -d '
@@ -25,11 +28,15 @@ alias gdh='git diff --color HEAD'
 alias gds='git diff --stat --color'
 alias gdsh1='git diff --stat --color "HEAD^"'
 alias gdsh='git diff --stat --color HEAD'
-alias gdst='git diff --color --staged'
+alias gdst='git diff --staged --color'
+
+alias gf='git fetch origin'
 
 alias gl='git log --pretty=format:"%C(yellow)%h %Cblue%ad %Creset%s" --date=short --all'
 alias glg='git log --graph --oneline --decorate --all --color'
 alias gll='git log --color'
+alias gls='git ls-files'
+alias glsi='git ls-files --others --i --exclude --standard'
 
 alias gs='git status'
 alias gss='git status -s'
@@ -43,34 +50,15 @@ alias gsts='git stash show'
 
 alias gm='git merge'
 alias gpic='git cherry-pick'
-alias gpul='git pull'
-alias gpus='git push'
-alias gpusforce='git push --force-with-lease'
+alias gpul='git pull origin '
+alias gpus='git push -u origin '
+alias gpusforce='git push --force-with-lease -u origin '
 alias greb='git rebase'
 alias grebi='git rebase -i'
 alias gref='git reflog'
 alias gres='git reset'
 alias grev='git revert'
 alias gt='git tag'
-# }}}
-
-# Advanced shortcuts {{{
-# cf. [mathiasbynens/dotfiles](https://github.com/mathiasbynens/dotfiles/blob/master/.gitconfig)
-# Find branches containing commit
-gfb() { git branch -a --contains "$1" }
-
-# Find tags containing commit
-gft() { git describe --always --contains "$1" }
-
-# Find commits by source code
-gfc() { git log --pretty=format:'%C(yellow)%h %Cblue%ad  %Creset%s %Cgreen%cn %Cred%d' --decorate --date=short -S"$1" }
-
-# Find commits by commit message
-gfm() { git log --pretty=format:'%C(yellow)%h %Cblue%ad  %Creset%s %Cgreen%cn %Cred%d' --decorate --date=short --grep="$1" }
-
-# Interactive rebase with the given number of latest commits
-grebn() { git rebase -i HEAD~$1 }
-# }}}
 # }}}
 
 # vim:set ft=zsh:
