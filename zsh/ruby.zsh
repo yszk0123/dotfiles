0a0b1e4b020ac98eba31d209a56485f2e8d78bf7
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-08-12 02:54:38 +0900

    fix(zsh): don't use lazy load for rbenv

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,17 +1,5 @@
 if is_exists 'rbenv'; then
-  function load_ruby() {
-    unset -f rbenv
-    unset -f ruby
-    unset -f irb
-    unset -f bundle
-
-    eval "$(rbenv init -)"
-  }
-
-  rbenv() { load_ruby; rbenv "$@"; }
-  ruby() { load_ruby; ruby "$@"; }
-  irb() { load_ruby; irb "$@"; }
-  bundle() { load_ruby; bundle "$@"; }
+  eval "$(rbenv init -)"
 
   alias be='bundle exec'
   alias berc='bundle exec rails c'
