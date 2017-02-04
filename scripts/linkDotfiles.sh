518db8647497b978e86b2b8f795ed07a3f9be279
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-02-04 20:00:05 +0900

    style(scripts): lint

--- scripts/linkDotfiles.sh
+++ scripts/linkDotfiles.sh
@@ -7,7 +7,7 @@
 
 # $ZDOTDIRが.zprofile中で指定される場合は、$HOME/.zprofileが読み込まれる（はず）
 # 念のため両方に用意しておく
-: ${ZDOTDIR:=~/.zsh}
+: "${ZDOTDIR:=~/.zsh}"
 ln -sni ~/dotfiles/.zprofile ~/.zprofile
 ln -sni ~/dotfiles/.zprofile "$ZDOTDIR/.zprofile"
 ln -sni ~/dotfiles/.zshrc "$ZDOTDIR/.zshrc"
@@ -16,9 +16,9 @@ for target in .bundle .git-template .ansible.cfg .ctags .remarkrc .gitconfig .pe
   ln -sni ~/dotfiles/$target ~/$target
 done
 
-: ${XDG_CONFIG_HOME:=~/.config}
-mkdir -p $XDG_CONFIG_HOME/nvim
-ln -sni ~/dotfiles/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+: "${XDG_CONFIG_HOME:=~/.config}"
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+ln -sni ~/dotfiles/.vimrc "$XDG_CONFIG_HOME/nvim/init.vim"
 
 if [ -f "$HOME/dotfiles/my_local_mode" ]; then
   for target in .gemrc .gvimrc .irbrc; do
