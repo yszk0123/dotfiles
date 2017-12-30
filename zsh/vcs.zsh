b7c6fbbcb2aa7f75d45a46ec3e43b7e5d8465d70
Yuji Suzuki <y0suzuki0123@gmail.com>
2017-12-30 22:01:36 +0900

    feat(zsh): remove unused aliases

--- zsh/vcs.zsh
+++ zsh/vcs.zsh
@@ -4,32 +4,19 @@
 # Aliases {{{
 alias g=git
 alias ga='git add'
-alias gaA='git add -A'
 alias gau='git add -u'
 alias ge='git exec '
 
 alias gb='git branch'
-alias gbd='git branch -d '
 
 alias gc='git commit'
 alias gca='git commit --amend'
-alias gcaar='git commit -a --amend --reuse-message=HEAD'
 alias gcar='git commit --amend --reuse-message=HEAD'
-alias gcm='git commit -m'
-alias gcp='git commit --patch'
-gcf() { git commit -m "fixup! $1" }
-gcs() { git commit -m "squash! $1" }
 
 alias gco='git checkout'
-alias gcob='git checkout -b'
-alias gcom='git checkout master'
 
 alias gd='git diff --color'
-alias gdh1='git diff --color "HEAD^"'
-alias gdh='git diff --color HEAD'
 alias gds='git diff --stat --color'
-alias gdsh1='git diff --stat --color "HEAD^"'
-alias gdsh='git diff --stat --color HEAD'
 alias gdst='git diff --staged --color'
 
 alias gf='git fetch origin'
@@ -45,7 +32,6 @@ alias gss='git status -s'
 
 alias gst='git stash'
 alias gsta='git stash apply'
-alias gstdrop='git stash drop'
 alias gstl='git stash list'
 alias gstp='git stash pop'
 alias gsts='git stash show'
@@ -57,10 +43,7 @@ alias gpus='git push -u origin '
 alias gpusforce='git push --force-with-lease -u origin '
 alias greb='git rebase'
 alias grebi='git rebase -i'
-alias gref='git reflog'
 alias gres='git reset'
-alias grev='git revert'
-alias gt='git tag'
 # }}}
 
 # vim:set ft=zsh:
