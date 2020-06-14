d0997abd3ebbdcac312bdb635943993bac162953
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-06-15 01:05:23 +0900

    Move scripts/how-to-* to howto/shell/

--- howto/shell/howto-inspect-active-port.sh
+++ howto/shell/howto-inspect-active-port.sh
@@ -1,2 +1,3 @@
+#!/bin/bash -E
 # cf. [macでLISTENとして使用しているポートを調べる方法 - Qiita](http://qiita.com/yokozawa/items/dbcb3b31f9308e4dcefc)
-`$ sudo lsof -i -P | grep "LISTEN"`
+sudo lsof -i -P | grep "LISTEN"
