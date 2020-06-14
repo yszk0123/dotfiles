c7667399b7371ac64a82312a903875659a745800
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-06-15 06:05:45 +0900

    Move brew.sh to Brewfile

--- scripts/install/brew.sh
+++ scripts/install/brew.sh
@@ -1,27 +1,4 @@
 #!/bin/bash
-
-BREW_LIST="$(brew list)"
-
-function is_installed() {
-  local result
-  result=$(echo "$BREW_LIST" | grep "^$1$")
-
-  if [ -z "$result" ]; then
-    return 1;
-  else
-    return 0;
-  fi
-}
-
-function confirm_then_brew_install() {
-  local target="$1"
-  is_installed "$target" && return
-
-  if confirm_with_message "Do you wish to install $target?"; then
-    brew install "$target"
-  fi
-}
-
 if ! is_exists "brew"; then
   if confirm_with_message "Do you wish to install brew?"; then
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -31,9 +8,3 @@ fi
 if ! is_exists "brew-file"; then
   brew install rcmdnk/file/brew-file
 fi
-
-confirm_then_brew_install tree
-confirm_then_brew_install rename
-confirm_then_brew_install ripgrep
-confirm_then_brew_install hub
-confirm_then_brew_install tig
