15569f77d94ba419ed5a503dc9207a1ab66d9d72
Yuji Suzuki <y0suzuki0123@gmail.com>
2020-06-15 01:08:31 +0900

    Remove unused git/

--- git/setup-gitlab-repo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-[[ ! -d ".git" ]] && echo 'Not a git repository' && exit 1
-[[ -n "$(git remote)" ]] && echo 'remote origin is already set. removing ...' && git remote remove origin
-set -eu
-
-dir="$(basename $(pwd))"
-git remote add origin git@gitlab.com:$(whoami)/$dir.git
-git push -u origin master
