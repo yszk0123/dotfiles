ef501d7a2a9a851487047a5157933643e8e2405b
Yuji Suzuki <y0suzuki0123@gmail.com>
2025-06-22 18:17:37 +0900

    fix(mise): fix path

--- zsh/mise.zsh
+++ zsh/mise.zsh
@@ -1,11 +1,13 @@
 export MISE_SHELL=zsh
 export __MISE_ORIG_PATH="$PATH"
 
+MISE_PATH="$HOME/.local/bin/mise"
+
 mise() {
   local command
   command="${1:-}"
   if [ "$#" = 0 ]; then
-    command /opt/homebrew/bin/mise
+    command "$MISE_PATH"
     return
   fi
   shift
@@ -14,16 +16,16 @@ mise() {
   deactivate|s|shell)
     # if argv doesn't contains -h,--help
     if [[ ! " $@ " =~ " --help " ]] && [[ ! " $@ " =~ " -h " ]]; then
-      eval "$(command /opt/homebrew/bin/mise "$command" "$@")"
+      eval "$(command "$MISE_PATH" "$command" "$@")"
       return $?
     fi
     ;;
   esac
-  command /opt/homebrew/bin/mise "$command" "$@"
+  command "$MISE_PATH" "$command" "$@"
 }
 
 _mise_hook() {
-  eval "$(/opt/homebrew/bin/mise hook-env -s zsh)";
+  eval "$("$MISE_PATH" hook-env -s zsh)";
 }
 typeset -ag precmd_functions;
 if [[ -z "${precmd_functions[(r)_mise_hook]+1}" ]]; then
@@ -39,7 +41,7 @@ if [ -z "${_mise_cmd_not_found:-}" ]; then
     [ -n "$(declare -f command_not_found_handler)" ] && eval "${$(declare -f command_not_found_handler)/command_not_found_handler/_command_not_found_handler}"
 
     function command_not_found_handler() {
-        if /opt/homebrew/bin/mise hook-not-found -s zsh "$1"; then
+        if "$MISE_PATH" hook-not-found -s zsh "$1"; then
           _mise_hook
           "$@"
         elif [ -n "$(declare -f _command_not_found_handler)" ]; then
